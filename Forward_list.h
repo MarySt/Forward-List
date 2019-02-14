cbd354f609260d54c1fe3e1e85459e8c494c49f3
MarySt <47612154+MarySt@users.noreply.github.com>
2019-02-14 05:01:48 +0200

    Add files via upload

---
+++ Forward_list.h
@@ -0,0 +1,69 @@
+#pragma once
+#include<memory>
+#include<stdexcept>
+
+template<typename T>
+class Forward_list
+{
+public:
+	Forward_list();
+	~Forward_list();
+	void push_front(T data);
+	void pop_front();
+	T& front() const;
+	bool empty() const;
+	size_t getSize() { return size; }
+private:
+	template<typename T>
+	class Node {
+	public:
+		T data;
+		std::shared_ptr<Node> pNext;
+		Node(T d, std::shared_ptr<Node<T>> next = nullptr) : data(d), pNext(next) {}
+	};
+	std::shared_ptr<Node<T>> head;
+	size_t size;
+};
+
+template<typename T>
+Forward_list<T>::Forward_list() : head(nullptr), size(0) {
+
+}
+
+template<typename T>
+Forward_list<T>::~Forward_list() {
+
+}
+
+template<typename T>
+inline void Forward_list<T>::push_front(T data)
+{
+	if (!head) {
+		head = std::make_shared<Node<T>>(data);
+	}
+	else {
+		auto curr = head;
+		head = std::make_shared<Node<T>>(data);
+		head->pNext = curr;
+	}
+	this->size++;
+}
+
+template<typename T>
+inline void Forward_list<T>::pop_front()
+{
+	head =head->pNext;
+	size--;
+}
+
+template<typename T>
+inline T & Forward_list<T>::front() const
+{
+	return head->data;
+}
+
+template<typename T>
+inline bool Forward_list<T>::empty() const
+{
+	return !this->size;
+}
---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿// forward_list.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include<forward_list>
+#include "Forward_list.h"
+
+int main()
+{
+	Forward_list<int> mylist;
+	mylist.push_front(10);
+	mylist.push_front(20);
+	mylist.push_front(30);
+	mylist.push_front(40);
+	mylist.push_front(50);
+	std::cout << mylist.front() << std::endl;
+	if (!mylist.empty()) std::cout << "Not empty!" << std::endl;
+	return 0;
+}
+
